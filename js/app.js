004d7c269d37aa04a837b330ff3a242ef277cad3
UcheEke <uche.eke@gmail.com>
2016-03-16 23:39:08 +0000

    first commit

---
+++ js/app.js
@@ -0,0 +1,57 @@
+/**
+ * Created by ekeu on 16/03/16.
+ */
+
+document.addEventListener('DOMContentLoaded', function(){
+    var input = document.getElementById("input");
+    var info = document.getElementById("info");
+
+    changeBtnColor = function(){
+        input.classList.remove('btn-success');
+        input.classList.add('btn-danger');
+        input.innerHTML="Stopped";
+        input.setAttribute('disabled', 'disabled');
+        setTimeout(function(){
+            input.classList.remove('btn-danger');
+            input.classList.add('btn-primary');
+            input.innerHTML="Press Me!";
+            input.removeAttribute('disabled');
+        }, 1000);
+    };
+
+    input.addEventListener('mousedown', function(){
+        // Change the display of the button
+        input.innerHTML = "Holding...";
+        input.classList.remove("btn-primary");
+        input.classList.add("btn-success");
+        // Create a new promise
+        var clicking = new Promise(function(resolve, reject){
+            var start = new Date();
+
+            // Success - call 'resolve'
+            input.onmouseup = function(){
+                resolve(new Date() - start);
+            };
+
+            // Failure  - call 'reject'
+            input.onmouseout = function(){
+                reject('You left the button!');
+            };
+        });
+
+        // then(resolve, reject) - we'll define them inline...
+        clicking.then(function(duration){
+            var tsec = (duration/1000);
+            if ( tsec <= 4.5 || tsec >= 5.5 ){
+                info.innerHTML = "<span class='red'>Fail!!: " + tsec + " secs</span>";
+            } else {
+                info.innerHTML = "<span class='green'>Success!!: " + tsec + " secs</span>";
+            }
+            setTimeout(changeBtnColor(),1000);
+        }, function(message){
+          window.alert("Challenge incomplete: " + message);
+        });
+    });
+
+
+});
